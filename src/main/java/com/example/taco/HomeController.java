80710008d06fff7403bff753687e8eb230442a2b
PrakrutiChandak <prakrutichandak@gmail.com>
2021-07-17 16:18:59 +0530

    controller added

---
+++ src/main/java/com/example/taco/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.taco;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
